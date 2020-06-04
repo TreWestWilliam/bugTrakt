b0c0a907b84cd10a3607edb8f438bae1989de156
TreWestWilliam <66348444+TreWestWilliam@users.noreply.github.com>
2020-06-03 21:10:26 -0400

    Further Structure
    This all holds my further structure for the next few days of development

---
+++ tracker/head.php
@@ -0,0 +1,20 @@
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+<header>
+        <nav>
+        <div id="headTop">
+            <div id="headLeft">
+                <h1>Home Button</h1>
+            </div>
+            <div id="headRight">
+            <ul id="HorizontalList">
+                <li><a href=http://localhost/BugTrakt/tracker/index.php>Your Home</a></li>
+                <li><a href="http://localhost/BugTrakt/about.php">Ticket List</a></li>
+                <li><a href="http://localhost/BugTrakt/tracker/admin/index.php">Admin</a></li>
+                <li><a href="http://localhost/BugTrakt/auth/logout.php">Log out</a></li>
+                </ul>
+            </div>
+        </div>
+            </nav>
+    </header>
---
+++ tracker/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+	<meta charset="utf-8">
+	<title>BugTrakt Tracker</title>
+	<meta name="author" content="William West">
+	<meta name="description" content="A simple web bug tracker">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../style.css">
+	<link rel="icon" type="image/x-icon" href=""/>
+</head>
+
+<body>
+<?php 
+    //calling our header
+    echo file_get_contents("head.php");
+    ?>
+    
+	<main>
+        
+    </main>
+    <?php 
+    //calling our footer
+    echo file_get_contents("foot.php");
+    ?>
+    
+	<script type="text/javascript" src=""></script>
+</body>
+
+</html>
