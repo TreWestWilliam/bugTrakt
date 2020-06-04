680038cd6abef67b822bdac819e47b3aea41010d
TreWestWilliam <66348444+TreWestWilliam@users.noreply.github.com>
2020-06-04 16:09:33 -0400

    Implemented a Mock index for logged in users

--- tracker/index.php
+++ tracker/index.php
@@ -8,6 +8,7 @@
 	<meta name="description" content="A simple web bug tracker">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="trackerStyle.css">
 	<link rel="icon" type="image/x-icon" href=""/>
 </head>
 
@@ -18,10 +19,84 @@
     ?>
     
 	<main>
-        
+        <p>This page will be dynamic in the future</p>
+        <div id="bodyContainer">
+            <div id="bodyLeft">
+                <h1>Your Tickets</h1>
+                <!-- Your tickets will be displayed here-->
+            <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>Created by: $Created_User_name Created on:$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+                <div id="yourTicket">
+            <h2>$TicketID | $TicketName</h2>
+                <p>$dateTime_created</p>
+            </div>
+            </div>
+            <div id="bodyRight"> 
+            <span id="alignCenter">    <h1>Recent updates</h1></span>
+                <div id="update">
+                    <h1>$updateTitle</h1>
+                    <p>text here</p>
+                </div>
+                <div id="update">
+                    <h1>Update 2.0.4x going live</h1>
+                    <p>On January 20th 2020 we will be pushing Update 2.0.4x.  All updates done from today onwards will be included in update 2.0.5.</p>
+                </div>
+                <a><p id="alignCenter">More updates here.</p></a>
+                <h1 id="alignCenter">Recently made tickets</h1>
+                
+                <div id="recentTicket">
+                <h2>$TicketID | $TicketName</h2>
+                    <p>Created by: $Created_User_name Created on:$dateTime_created</p>
+                </div>
+                <div id="recentTicket">
+                <h2>$TicketID | $TicketName</h2>
+                    <p>Created by: $Created_User_name Created on:$dateTime_created</p>
+                </div>
+                
+            </div>
+        </div>
     </main>
     <?php 
     //calling our footer
+    chdir("..");
     echo file_get_contents("foot.php");
     ?>
     
